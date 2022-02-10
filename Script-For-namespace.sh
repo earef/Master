5052d3e3b6a895c0d8737e5ee66e3186247e3711
earef <elham.aref@hotmail.com>
2022-02-10 18:36:34 +0100

    New version

--- Script-For-namespace.sh
+++ Script-For-namespace.sh
@@ -1,27 +1,41 @@
-##!/bin/bash
-
-ip netns add A ;
-ip netns add Z ; #Creates A and Z networks
-#ovs-vsctl add-br OVS1 ; #creates the connection tube
-echo "A and Z Are created"
-ip link add eth0-A type veth peer name veth-A ;
-ip link set eth0-A netns A ;
-ip link ;
-ip netns exec A ip link ;
-
-ovs-vsctl add-port OVS1 veth-A ;
-ip link add eth0-Z type veth peer name veth-Z ;
-
-ovs-vsctl add-port OVS1 veth-Z ;
-ip link set veth-A up ;
-
-ip netns exec A ip link set dev lo up ;
-ip netns exec A ip link set dev eth0-A up ;
-ip netns exec A ip address add 10.0.0.1/24 dev eth0-A ;
-
-ip link set dev veth-Z up ;
-ip netns exec Z bash ;
-ip link set dev lo up ;
-ip link set dev eth0-Z up ;
-ip address add 10.0.0.2/24 dev eth0-Z ;
-ip netns exec A bash
+#ip link comment
+#ip a
+#ip route
+ip netns add red
+ip netns add green
+#ip netns
+# ls /var/run/netns
+#delete ip netns del red
+#ip netns exec red ip link
+#ip netns exec green ip link
+ovs-vsctl add-br OVS1
+#ovs-vsctl show
+#ip link
+ip link add eth0-r type veth peer name veth-r
+#ip link
+ip link set eth0-r netns red
+ip link
+#ip netns exec red ip link
+ovs-vsctl add-port OVS1 veth-r
+#ovs-vsctl show
+ip link add eth0-g type veth peer name veth-g
+ip link set eth0-g netns green
+ovs-vsctl add-port OVS1 veth-g
+ip link
+ip link set veth-r up
+ip link
+ip netns exec red ip link set dev lo up
+ip netns exec red ip link set dev eth0-r up
+ip netns exec red ip address add 10.0.0.3/24 dev eth0-r
+ip netns exec red ip a
+ip netns exec red ip route
+ip route
+ip link set dev veth-g up
+ip netns exec green bash
+ip link set dev lo up
+ip link set dev eth0-g up
+ip address add 10.0.0.4/24 dev eth0-g
+ip a
+ip netns exec red bash
+#ping
+Exit
