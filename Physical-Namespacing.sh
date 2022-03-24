19e7dd318d2c8fc7efee4a8115d33a1bb79df7cf
earef <elham.aref@hotmail.com>
2022-03-24 20:38:02 +0100

    start and end added name correction done.

--- Physical-Namespacing.sh
+++ Physical-Namespacing.sh
@@ -1,5 +1,6 @@
-ip netns add net1;
-ip netns add net2;
+#!/bin/bash
+ip netns add ns1;
+ip netns add ns2;
 ip netns exec ns1 ip link set dev lo up;
 ip netns exec ns1 ip address;
 ip link set eth0 netns ns1;
@@ -18,3 +19,4 @@ ip netns exec ns2 ip address show wlan0;
 ip netns exec ns2 ip link set dev wlan0 up;
 ip netns exec ns2 ip address show wlan0 ;
 ip netns exec ns2 ip link set wlan0 netns 1 ;
+Exit
